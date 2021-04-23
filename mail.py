d4911ddc30ac82769adf6c6503ed2b860cc8b134
aditya <adityay186@gmail.com>
2021-04-23 13:01:21 +0530

    Tweaks.

--- mail.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login("adityactms@gmail.com", "myselfaditya")
-message = "Your Search Results Are Ready"
-s.sendmail("adityactms@gmail.com", "adityay186@gmail.com", message)
-s.quit()
